86e060c9b49bb3a54f9a257cde85f83416037fff
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2021-02-08 15:25:31 +0900

    FIX: link the secondary files of the reference to the woking directory

--- per-sample/docker/gridss/gridss-germline.sh
+++ per-sample/docker/gridss/gridss-germline.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 ln -s $REFERENCE .
+ln -s $REFERENCE.amb .
+ln -s $REFERENCE.ann .
+ln -s $REFERENCE.bwt .
+ln -s $REFERENCE.pac .
+ln -s $REFERENCE.sa .
+ln -s $REFERENCE.alt .
+ln -s $REFERENCE.fai .
+
 /opt/gridss/gridss.sh \
     $CRAM \
     -o $VCF \
     -a $ASSEMBLY \
-    -r $REFERENCE \
+    -r $(basename $REFERENCE) \
     -t $NUM_THREADS \
     --picardoptions VALIDATION_STRINGENCY=LENIENT \
     --jvmheap $JVM_HEAP
