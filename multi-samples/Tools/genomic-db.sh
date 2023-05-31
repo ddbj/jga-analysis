6770ea4af1420e342eea3aa74aa8ef5a4e39e938
Manabu Ishii <manabu.ishii.rb@gmail.com>
2023-05-31 13:45:29 +0900

    Update glob pattern for genomic-db

--- multi-samples/Tools/genomic-db.sh
+++ multi-samples/Tools/genomic-db.sh
@@ -8,8 +8,23 @@ INPUTFILESDIR=${@:$#:1}
 # ls -l ${INPUTFILESDIR}
 # echo "---- list INPUTFILESDIR/*.g.vcf.gz"
 # ls -l ${INPUTFILESDIR}/*.g.vcf.gz
+
+GLOBPATTERN=*.g.vcf.gz
+for I in "${@}"; do
+  if  [[ ${I} == *.bed ]]
+  then
+    if [[ ${I} == *chrX_nonPAR_ploidy_1* ]]
+    then
+      GLOBPATTERN=*.chrX_nonPAR_ploidy_1.g.vcf.gz
+    elif [[ ${I} == *chrY_nonPAR_ploidy_1* ]]
+    then
+      GLOBPATTERN=*.chrY_nonPAR_ploidy_1.g.vcf.gz
+    fi
+  fi
+done
+
 INPUTFILEOPTIONS=""
-for FILE in $(ls ${INPUTFILESDIR}/*.g.vcf.gz)
+for FILE in $(ls ${INPUTFILESDIR}/${GLOBPATTERN})
 do
   INPUTFILEOPTIONS+=" -V ${FILE}"
 done
@@ -20,3 +35,4 @@ done
 
 #ls -l /
 #echo $INPUTFILEOPTIONS
+
