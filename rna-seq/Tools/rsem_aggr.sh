27714805300d0b39a5b11a557ba54fb968c4054f
yamaken37 <yamamotokentarou@yamakenM1MBP-GAJ.local>
2023-10-13 17:07:37 +0900

    rsem_aggr.sh Variable argument reception

--- rna-seq/Tools/rsem_aggr.sh
+++ rna-seq/Tools/rsem_aggr.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+#!/bin/bash
+
+# Initialize variables
+ISOFORMS=()
+GENES=()
+PREFIX=""
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i)
+            shift
+            while [[ "$#" -gt 0 && "$1" != -* ]]; do
+                ISOFORMS+=("$1")
+                shift
+            done
+            ;;
+        -g)
+            shift
+            while [[ "$#" -gt 0 && "$1" != -* ]]; do
+                GENES+=("$1")
+                shift
+            done
+            ;;
+        -p)
+            shift
+            PREFIX="$1"
+            shift
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Echo results
+echo "-i: ${ISOFORMS[@]}"
+echo "-g: ${GENES[@]}"
+echo "-p: $PREFIX"
+
+
 
 # ====STEP1=====
 # 変数の読み込み rsem_isoforms:の配列
@@ -10,7 +52,7 @@
 
 # ======STEP2=======
 # $なんちゃらをシェル内の変数に変更
-echo $(date +"[%b %d %H:%M:%S] Combining transcript-level output")
-python3 /TSCA/ccle_processing/RNA_pipeline/aggregate_rsem_results.py $1 TPM IsoPct expected_count $2
-echo $(date +"[%b %d %H:%M:%S] Combining gene-level output")
-python3 /TSCA/ccle_processing/RNA_pipeline/aggregate_rsem_results.py $3 TPM expected_count $2
+# echo $(date +"[%b %d %H:%M:%S] Combining transcript-level output")
+# python3 /TSCA/ccle_processing/RNA_pipeline/aggregate_rsem_results.py $1 TPM IsoPct expected_count $2
+# echo $(date +"[%b %d %H:%M:%S] Combining gene-level output")
+# python3 /TSCA/ccle_processing/RNA_pipeline/aggregate_rsem_results.py $3 TPM expected_count $2
