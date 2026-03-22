e718d6fb2b24d3eb48dc92515a69d5bde090f65c
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2026-03-22 18:06:17 +0900

    Refactor

--- germline-graph/script/modify_and_merge_vcf.sh
+++ germline-graph/script/modify_and_merge_vcf.sh
@@ -12,9 +12,9 @@ TEMP=$(mktemp -d)
 for VCF_GZ in "$@"
 do
     BASENAME=$(basename "$VCF_GZ" .vcf.gz)
-    bcftools view -H "$VCF_GZ" | cut -f 1-4 | sort -k 1,1 -k 2,2n -k3,3 > "$TEMP"/"$BASENAME".pos.tsv
+    bcftools view -H "$VCF_GZ" | cut -f 1-4 | sort -k 1,1 -k 2,2n -k 3,3 > "$TEMP"/"$BASENAME".pos.tsv
 done
-sort -k 1,1 -k 2,2n -k3,3 -m -u "$TEMP"/*.pos.tsv > "$OUT_PREFIX".pos.tsv
+sort -k 1,1 -k 2,2n -k 3,3 -m -u "$TEMP"/*.pos.tsv > "$OUT_PREFIX".pos.tsv
 sort -k 3,3 -s "$OUT_PREFIX.pos.tsv" > "$OUT_PREFIX.pos.id_sort.tsv"
 awk 'BEGIN {FS=OFS="\t"} {print $1,$2,$4,$3}' "$OUT_PREFIX.pos.id_sort.tsv" | uniq -f 3 -d > "$OUT_PREFIX.pos.dup.tsv"
 
